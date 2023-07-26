41940e3cd0f7345d00fd59d45161302b59afde3a
sachinraj31 <sachinraj31011998@gmail.com>
2023-07-26 14:19:26 +0530

    movie form added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
 import classes from "./Home.module.css";
+import MovieForm from "../../Components/UI/MovieForm/MovieForm";
 
 const Home = () => {
-  return <div className="container fw-bold fs-1">Home</div>;
+  return (
+    <div className="container fw-bold fs-1">
+      <MovieForm></MovieForm>
+    </div>
+  );
 };
 
 export default Home;
